4aae93a3348ef0f0c93b9763406d277596b1f482
DESKTOP-24J1846\Stile <branimir.stilinovic@outlook.com>
2021-01-19 12:56:52 +0100

    Player Controller

---
+++ Assets/Scripts/BetterJumping.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterJumping : MonoBehaviour
+{
+    Rigidbody2D _rigidBody2D;
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    private void Awake()
+    {
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate()
+    {
+
+        if (_rigidBody2D.velocity.y < 0)
+        {
+            _rigidBody2D.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (_rigidBody2D.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            _rigidBody2D.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator _animator;
+    Rigidbody2D _rigidBody2D;
+
+    [Header("Movement")]
+    public float MovementSpeed = 2.0f;
+    public float _movementHorizontal;
+    private bool _isFacingRight = true;
+
+
+    [Header("Jummping")]
+    public float JumpVelocity = 5f;
+    public bool isGrounded = true;
+    public Transform GroundCheck;
+    public LayerMask groundLayerMask;
+
+    [Header("Wall Sliding")]
+    public Transform WallCheck;
+    public Vector2 wallCheckSize;
+    public float wallSlideSpeed = 0;
+    public LayerMask wallLayerMask;
+    public bool isTouchingWall;
+    public bool isWallSliding;
+
+    [Header("Wall Jumping")]
+    public float wallJumpForce;
+
+
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        WorldCheck();
+        Move();
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 localScale = transform.localScale;
+
+        if (_movementHorizontal > 0.0f)
+            _isFacingRight = true;
+        else if (_movementHorizontal < 0.0f)
+            _isFacingRight = false;
+
+        if (((_isFacingRight) && (localScale.x < 0.0f))
+             ||
+             ((!_isFacingRight) && (localScale.x > 0.0f))
+            )
+            localScale.x *= -1.0f;
+
+        transform.localScale = localScale;
+    }
+
+    private void FixedUpdate()
+    {
+        Jump();
+        WallSlide();
+    }
+
+    public void WorldCheck()
+    {
+        isGrounded = Physics2D.Linecast(transform.position, GroundCheck.position, groundLayerMask);
+        isTouchingWall = Physics2D.OverlapBox(WallCheck.position, wallCheckSize, 0, wallLayerMask);
+
+    }
+
+    private void Move()
+    {
+        _movementHorizontal = Input.GetAxis("Horizontal");
+
+        _rigidBody2D.velocity = new Vector2(_movementHorizontal * MovementSpeed, _rigidBody2D.velocity.y);
+        _animator.SetBool("IsRunning", _movementHorizontal != 0.0f);
+    }
+
+    public void Jump()
+    {
+        if (Input.GetButton("Jump") && isGrounded)
+            _rigidBody2D.velocity = Vector2.up * JumpVelocity;
+    }
+
+    private void WallSlide()
+    {
+        if (isTouchingWall && !isGrounded && _rigidBody2D.velocity.y < 0)
+            isWallSliding = true;
+        else
+            isWallSliding = false;
+
+        if (isWallSliding)
+            _rigidBody2D.velocity = new Vector2(_rigidBody2D.velocity.x, wallSlideSpeed);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, GroundCheck.position);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(WallCheck.position, wallCheckSize);
+
+
+    }
+
+}
